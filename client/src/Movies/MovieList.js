ce222078ff9c5ebf403466f4d1fa720bcc8f6f4d
Chase Wex <clapingmanly@hotmail.com>
2019-05-07 18:31:34 -0400

    removed useless code that was recalling the data from the server

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import {Link} from 'react-router-dom';
 import MovieCard from './MovieCard';
 
 
 export default class MovieList extends Component {
-  constructor(props) {
+/*   constructor(props) {
     super(props);
     this.state = {
       movies: []
     };
-  }
-
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/api/movies')
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
-  }
+  } */
 
   render() {
     return (
       <div className="movie-list">
-        {this.state.movies.map(movie => (
+        {this.props.movies.map(movie => (
           <MovieDetails key={movie.id} movie={movie} id={movie.id} />
         ))}
       </div>
